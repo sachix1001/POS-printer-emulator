b9fd5f036ce6dd42746d42cfd0703417c766f3d2
sachix1001 <sachix1001@gmail.com>
2020-04-18 11:04:47 +1000

    set api GET

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port =  process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/cgi-bin/epos/service.cgi?devid=test&timeout=35000", (req, res) => {
+app.get("/cgi-bin/epos/service.cgi?devid=internalIP&timeout=35000", (req, res) => {
     res.send("Hello World")
 })
+
+
+app.listen(port, () =>
+console.log("app listening on port " + port))
