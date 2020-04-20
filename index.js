44269e6ff89e753cba35a573efa893a2b363dc37
sachix1001 <sachix1001@gmail.com>
2020-04-20 11:27:49 +1000

    add response

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const xmlparser = require('express-xml-bodyparser');
+
 const app = express();
-const port =  process.env.PORT || 3000;
+app.use(xmlparser());
+const port =  process.env.PORT || 80;
 
 app.use(express.json());
 
-app.get("/address", (req, res) => {
-    res.send("Hello Sachi")
+
+app.post("/cgi-bin/epos/service.cgi", (req, res) => {
+    let text;
+    const body = req.body["s:envelope"]["s:body"];
+    text = body[0]["epos-print"][0].text
+    text = text.map(item => item._)
+    console.log(text)
+    const response = 
+    `<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Header
+    <parameter xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print"> <devid>internalIP</devid>
+    <timeout>35000</timeout>
+    </parameter> </s:Header> <s:Body>
+    <response xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print" success="true" code="" status="252641302" battery="0" />
+    </s:Body> </s:Envelope>`
+    res.send(response)
 })
 
 
-app.listen(port, () =>
-console.log("app listening on port " + port))
+app.listen(port, () => console.log("app listening on port " + port))
